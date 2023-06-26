a2b8f8574ec3e6ff91253d72e3d3887e22899137
Phan Thanh Tùng <tungpt1@vmodev.com>
2023-06-26 18:45:16 +0700

    update code structure (need to add dbContext)

--- niteco-mvc-project/Controllers/HomeController.cs
+++ niteco-mvc-project/Controllers/HomeController.cs
@@ -1,31 +1,30 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using niteco_mvc_project.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-namespace niteco_mvc_project.Controllers;
-
-public class HomeController : Controller
+namespace niteco_mvc_project.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
