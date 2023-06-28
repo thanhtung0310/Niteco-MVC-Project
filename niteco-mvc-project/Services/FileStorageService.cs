10442af0e139b48960b394b53292a8a6202686bc
Phan Thanh Tùng <tungpt1@vmodev.com>
2023-06-28 17:52:13 +0700

    Feature: Create draft code for Logging and FileStorage Service

---
+++ niteco-mvc-project/Services/FileStorageService.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace niteco_mvc_project.Services
+{
+    public static class FileStorageService
+    {
+        //TODO: Service to save file to storage
+        public static string SaveFileToFilePath(byte[] fileBytes, string path)
+        {
+            File.WriteAllBytes(path, fileBytes);
+
+            return path;
+        }
+
+        public static string GetFilePathFromFileName(string fileName)
+        {
+            string rootPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string path = Path.Combine(rootPath, fileName);
+
+            return path;
+        }
+    }
+}
---
+++ niteco-mvc-project/Services/LoggingService.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Reflection;
+using System;
+
+namespace niteco_mvc_project.Services
+{
+    public static class LoggingService
+    {
+        private static string m_exePath = string.Empty;
+
+        public static void LogWrite(string logMessage, bool needFilePath = false)
+        {
+            m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var loggingFolderPath = m_exePath + "\\" + "Logs";
+            if (!Directory.Exists(loggingFolderPath))
+            {
+                Directory.CreateDirectory(m_exePath + "\\" + "Logs");
+            }
+            var currentDate = DateTime.Now;
+            string loggingFilePath = loggingFolderPath + "\\" + $"Log-{currentDate.Year}-{currentDate.Month}-{currentDate.Day}.txt";
+
+            try
+            {
+                using (StreamWriter w = File.AppendText(loggingFilePath))
+                {
+                    if (needFilePath)
+                    {
+                        var logFilePath = loggingFilePath.Replace("\\", "/");
+                        Log(logMessage, w, logFilePath);
+                    }
+                    else
+                    {
+                        Log(logMessage, w);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public static void Log(string logMessage, TextWriter txtWriter, string filePath = "")
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(filePath))
+                {
+                    txtWriter.WriteLine("{0} {1} {2} {3}",
+                        DateTime.Now.ToString("MM/dd/yyyy"),
+                        DateTime.Now.ToString("HH:mm:ss tt"),
+                        logMessage,
+                        filePath);
+                }
+                else
+                {
+                    txtWriter.WriteLine("{0} {1} {2}",
+                        DateTime.Now.ToString("MM/dd/yyyy"),
+                        DateTime.Now.ToString("HH:mm:ss tt"),
+                        logMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
