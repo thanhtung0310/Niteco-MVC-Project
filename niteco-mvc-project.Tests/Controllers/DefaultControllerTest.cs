d0f60bf851605ef888c975d7a8de478da49fb227
Phan Thanh Tùng <tungpt1@vmodev.com>
2023-07-02 16:21:53 +0700

    Feature: Create draft code for Unit test DefaultController

---
+++ niteco-mvc-project.Tests/Controllers/DefaultControllerTest.cs
@@ -0,0 +1,18 @@
+﻿namespace niteco_mvc_project.Tests.Controllers;
+
+[TestClass]
+public class DefaultControllerTest
+{
+    [TestMethod]
+    public void Index()
+    {
+        // Arrange
+        HomeController controller = new HomeController();
+
+        // Act
+        ViewResult result = controller.Index() as ViewResult;
+
+        // Assert
+        Assert.IsNotNull(result);
+    }
+}
